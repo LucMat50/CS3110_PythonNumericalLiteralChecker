c34d4b2882337886fe7528b8104fb7b4351111dd
LucMat50 <lmaturino5@gmail.com>
2025-11-11 16:43:59 -0800

    run function

--- CS3110 PythonNumericalLiteralChecker/decIntegerLiterals.py
+++ CS3110 PythonNumericalLiteralChecker/decIntegerLiterals.py
@@ -1,3 +1,7 @@
+#CS3110 - FORMAL LANGUAGES AND AUTOMATA
+#RECOGNIZING DECINTEGER
+#NAME: LUCIA MATURINO INIGUEZ
+
 #NFA CLASS we can use for all NFAs
 class NFA:
     def __init__(self, states, alphabet, transitions, start_state, accept_states):
@@ -15,3 +19,28 @@ class NFA:
 
         #Format: { "state": {"symbol": {next_state1, next_state2, ...}}}
         self.transitions = set(transitions)
+
+#RUN FUNCTION 
+# - Simulates input_string through NFA
+# - Returns ACCEPT or REJECT
+def run(self, input_string):
+    current_states = {self.start_state}
+
+    for symbol in input_string: 
+        if symbol not in self.alphabet:
+            return False
+        
+        next_states = set()
+        for state in current_states:
+            if symbol in self.transitions.get(state, {}):
+                next_states.update(self.transitions[state][symbol])
+        
+        current_states = next_states
+
+        if not current_states:
+            return False
+        
+    if bool(current_states.intersection(self.accept_states)) == True:
+        return "ACCEPT"
+    else: 
+        return "REJECT"

92163f1afcb4455591ca31a3555e94c4b4a5f3f3
LucMat50 <lmaturino5@gmail.com>
2025-11-12 21:27:16 -0800

    program now writes results to output file

--- CS3110 PythonNumericalLiteralChecker/decIntegerLiterals.py
+++ CS3110 PythonNumericalLiteralChecker/decIntegerLiterals.py
@@ -114,9 +114,11 @@ def main():
 
     #Text file with test inputs for decinteger NFA
     test_input = "CS3110 PythonNumericalLiteralChecker\decIntegerTestInput.txt"
+    test_output = "CS3110 PythonNumericalLiteralChecker\decIntegerTestOutput.txt"
 
     #Reads through each line of test_input file
     with open(test_input, 'r') as file:
+        result = ""
         for line in file:
 
             #Splits line for first space --> stores first word (the test input)
@@ -134,9 +136,15 @@ def main():
             else:
                 pass_fail = "fail"
             
+            #String is appended to result
+            result += f"TEST INPUT: {test_num} | EXPECTED RESULT: {expect_result} | ACTUAL RESULT: {actual_result} | PASS/FAIL: {pass_fail}\n"
+            result += "-----------------------------------------------------------------------------\n"
             #Prints out results
-            print(f"TEST INPUT: {test_num} | EXPECTED RESULT: {expect_result} | ACTUAL RESULT: {actual_result} | PASS/FAIL: {pass_fail}\n")
-            print("-----------------------------------------------------------------------------\n")
+            #print(f"TEST INPUT: {test_num} | EXPECTED RESULT: {expect_result} | ACTUAL RESULT: {actual_result} | PASS/FAIL: {pass_fail}\n")
+            #print("-----------------------------------------------------------------------------\n")
+    
+    with open(test_output, 'w') as file:
+        file.write(result)
 
 if __name__ == "__main__":
     main()
